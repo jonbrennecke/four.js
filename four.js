2e0a21afc81bc7f34d5af2302bca0cd9b11aa805
Jon Brennecke <jpbrennecke@gmail.com>
2014-10-22 10:26:27 -0700

    reworking the classes a bit

--- four.js
+++ four.js
@@ -1,246 +1,296 @@
+"use strict";
+"use asm";
+
+
+
 var FOUR = FOUR || {};
 
 
+
+// parameters for renderTargets
+FOUR.renderTargetParams = { 
+	minFilter: THREE.LinearFilter, 
+	magFilter: THREE.LinearFilter, 
+	format: THREE.RGBAFormat, 
+	stencilBufer: false 
+};
+
+
+
+// parameters for WebGLRenderer
+FOUR.webGLRendererParams = { 
+	antialias: true, 
+	alpha: true, 
+	preserveDrawingBuffer: true 
+};
+
+
+
 /**
- * /////////////////////////////////////////////////////////////////////////////////
+ * ////////////////////////////////////////////////////////////////////////
  *
- * 
+ *
+ * SHADER
+ *
+ * abstract base class (Don't instantiate directly!)
+ *
+ *
+ * ////////////////////////////////////////////////////////////////////////
+ */
+FOUR.Shader = function ( shader ) {
+	THREE.ShaderPass.call(this,shader);
+};
+FOUR.Shader.prototype = Object.create(THREE.ShaderPass.prototype)
+
+
+
+/**
+ *
+ * Depth of Field Shader
+ *
+ */
+FOUR.DepthOfFieldShader = function ( scene ) {
+
+	this.scene = scene;
+
+	scene
+		.renderer
+		.onPreRender
+		.progress(this.onPreRender.bind(this))
+	scene
+		.renderer
+		.onPostRender
+		.progress(this.onPostRender.bind(this))
+
+	this.uniforms = {
+		tDepth : { type: "t", texture: null },
+		tRender : { type: "t", texture: null },
+		znear : { type: "f", value : scene.camera.near },
+		zfar : { type: "f", value : scene.camera.far },
+		iResolution : { type: "v2", value : new THREE.Vector2(scene.width,scene.height) },
+		focalDepth : { type: "f", value: 10.5 },
+		focalLength : { type: "f", value: 2.5 },
+		fstop: { type: "f", value: 0.5 },
+		dithering : { type: "f", value: 0.0001 },
+		maxblur : { type: "f", value: 4.0 },
+		threshold : { type: "f", value: 4 },
+		gain : { type: "f", value: 1.0 },
+		bias : { type: "f", value: 1.0 },
+		fringe : { type: "f", value: 7 },
+	};
+
+	this.depthMaterial = new THREE.MeshDepthMaterial();
+	this.renderTarget = new THREE.WebGLRenderTarget(
+		scene.width, 
+		scene.height, 
+		FOUR.renderTargetParams);
+
+	this.vertexShader = $("#four--shader-depth-vs")[0].innerText;
+	this.fragmentShader = $("#four--shader-depth-fs")[0].innerText;
+
+	this.uniforms.tRender.value = scene.renderer.compositor.renderTarget2;
+	this.uniforms.tDepth.value = this.renderTarget;
+
+	FOUR.Shader.call(this,{
+		uniforms : this.uniforms,
+		vertexShader : this.vertexShader,
+		fragmentShader : this.fragmentShader
+	});
+};
+FOUR.DepthOfFieldShader.prototype = Object.create(FOUR.Shader.prototype);
+
+
+
+/**
+ *
+ * called before every call to the render callback
+ *
+ * render depth data to a buffer
+ *
+ */
+FOUR.DepthOfFieldShader.prototype.onPreRender = function () {
+	this.scene.overrideMaterial = depth.material;
+	this.scene.renderer.render(
+		this.scene, 
+		this.camera, 
+		this.renderTarget, 
+		true);
+}
+
+
+/**
+ *
+ * called after every call to the render callback
+ *
+ */
+FOUR.DepthOfFieldShader.prototype.onPostRender = function () {}
+
+
+
+/**
+ * ////////////////////////////////////////////////////////////////////////
+ *
+ *
  * COMPOSITOR
  *
- * inherits from THREE.EffectComposer (from effects.js)
  *
- * :param renderer - instance of 'FOUR.Renderer' 
- *
- * /////////////////////////////////////////////////////////////////////////////////
+ * ////////////////////////////////////////////////////////////////////////
  */
-FOUR.Compositor = function ( scene ) {
-	var renderTargetParameters = { 
-			minFilter: THREE.LinearFilter, 
-			magFilter: THREE.LinearFilter, 
-			format: THREE.RGBAFormat, 
-			stencilBufer: false
-		},
-		renderTarget = new THREE.WebGLRenderTarget(
-			scene.width, 
-			scene.height, 
-			renderTargetParameters);
+FOUR.Compositor = function ( renderer ) {
 
-	THREE.EffectComposer.call(this,scene.renderer.renderer, renderTarget);
+	this.renderer = renderer;
+
+	// *************************************************** 
+	// create a render pass
+	// ***************************************************
+
+	this.renderTarget = new THREE.WebGLRenderTarget(
+		renderer.scene.width, 
+		renderer.scene.height, 
+		FOUR.renderTargetParams
+	);
+
+	this.renderPass = new THREE.RenderPass( 
+		renderer.scene, 
+		renderer.scene.camera
+	);
+
+	this.renderPass.renderToScreen = true;
+
+	THREE.EffectComposer.call(this,renderer.__renderer,this.renderTarget);
+	this.addPass(this.renderPass);
 };
 FOUR.Compositor.prototype = Object.create(THREE.EffectComposer.prototype);
 
-FOUR.Compositor.prototype.update = function () {
-	for (var i = 0; i < this.passes.length; i++) {
-		this.passes[i].update();
-	};
-	return this;
-}
+
+
 
 /**
- * /////////////////////////////////////////////////////////////////////////////////
+ * ////////////////////////////////////////////////////////////////////////
  *
- * 
- * RENDERPASS
  *
- * inherits from THREE.RenderPass (from effects.js)
+ * Renderer
  *
- * :param shader - any of the child classes inheriting from 'FOUR.Shader'
  *
- * /////////////////////////////////////////////////////////////////////////////////
+ * ////////////////////////////////////////////////////////////////////////
  */
-FOUR.RenderPass = function () {
-	FOUR.DeferWithScene.call(this);
-	this.shader = undefined;
-
-	this.sceneAsync.then(function ( scene ) {
-		THREE.RenderPass.call(this,scene,scene.camera);
-	}.bind(this));
-};
-FOUR.RenderPass.prototype = Object.create(THREE.RenderPass.prototype);
-$.extend(FOUR.RenderPass.prototype,Object.create(FOUR.DeferWithScene.prototype));
-
-FOUR.RenderPass.prototype.update = function () {
-	// this.shader.update()
-	return this;
-}
-
-FOUR.RenderPass.prototype.addShader = function ( shader ) {
-	this.shader = shader;
-	return this;
-}
-
-FOUR.RenderPass.prototype.addTo = function ( renderer ) {
-	// this.sceneAsync.then(function(){
-	// 	// renderer.renderCallback()
-
-	// }.bind(this))
-
-	renderer.onRender(function(){
-		
-	});
-	return this;
-}
-
-/**
- * /////////////////////////////////////////////////////////////////////////////////
- *
- * 
- * RENDERER
- * 
- *
- * /////////////////////////////////////////////////////////////////////////////////
- */
-FOUR.Renderer = function ( parent ) {
-	this.parent = parent;
-
-	this.compositors = [];
-
-	THREE.WebGLRenderer.call(this,{ 
-		"antialias":true, 
-		"alpha":true, 
-		"preserveDrawingBuffer":true
-	});
-
-}
-FOUR.Renderer.prototype = Object.create(THREE.WebGLRenderer);
-
-FOUR.Renderer.prototype.addCompositor = function ( comp ) {
-	this.compositors.push(comp);
+FOUR.Renderer = function ( scene ) {
+	this.scene = scene;
+	this.onPreRender = $.Deferred();
+	this.onPostRender = $.Deferred();
+	this.isUsingShaders = false;
+	this.__renderer = new THREE.WebGLRenderer(FOUR.webGLRendererParams);
+	this.__renderer.setSize(scene.width, scene.height);
+	this.__renderer.shadowMapEnabled = true;
+	this.__renderer.setClearColor(scene.backgroundColor, 1);
+	$(this.__renderer.domElement).appendTo(scene.domElement);
+	this.compositor = new FOUR.Compositor(this);
 };
 
-FOUR.Renderer.prototype.set = function ( params ) {
-	this.setSize(params.width||640, params.height||480);
-	this.shadowMapEnabled = params.shadows;
-	this.setClearColor(params.backgroundColor, params.alpha||1);
-	$( this.domElement ).appendTo(params.parent||document.body);
+FOUR.Renderer.prototype = {
+	
+	/**
+	 *
+	 * 
+	 *
+	 */
+	render : function () {
+		this.onPreRender.notify();
+		if ( this.isUsingShaders ) { this.compositor.render(); } 
+		else { this.__renderer.render(this.scene,this.scene.camera); }
+		this.onPostRender.notify();
+	},
+
+
+	/**
+	 *
+	 * 
+	 *
+	 */
+	addShader : function ( shader ) {
+		if ( ! (shader instanceof FOUR.Shader) ) { /*TODO error*/ }
+		if ( !this.isUsingShaders ) {
+			this.isUsingShaders = true;
+			this.compositor.addPass(shader);
+		}
+
+	}
 };
-
-
-FOUR.Renderer.prototype.render = function ( scene, camera ) {
-	// if ( this.compositors.length ) {
-		// for (var i = 0; i < this.compositors.length; i++) {
-		// 	this.compositors[i].update().render();
-		// };
-	// }
-	// else { this.render(scene, camera); }
-}
-
 
 
 
 /**
- * /////////////////////////////////////////////////////////////////////////////////
+ * ////////////////////////////////////////////////////////////////////////
  *
  *
- * SCENE
+ * Scene
  *
- * inherits from THREE.Scene 
- * 
- * :description
- * Creates a THREE.js scene 
+ * :param options - json object with the (optional) fields:
+ * {
+ *			
+ * }
  *
- * /////////////////////////////////////////////////////////////////////////////////
+ *
+ * ////////////////////////////////////////////////////////////////////////
  */
 FOUR.Scene = function ( options ) {
-
-	// parent element + width and height
-	this.parent = $(options.parent || document.body);
-	this.height = this.parent.height();
-	this.width = this.parent.width();
-
+	this.domElement = $(options.parent || document.body);
+	this.height = this.domElement.height();
+	this.width = this.domElement.width();
 	this.backgroundColor = options.backgroundColor || new THREE.Color(0x000000);
-
-	// THREE.js scene objects
-	this.camera = new THREE.PerspectiveCamera( 50, this.width / this.height, 0.1, 100 );
+	this.camera = new THREE.PerspectiveCamera(50, this.width / this.height, 1, 10);
 	this.renderer = new FOUR.Renderer(this);
-
-	window.addEventListener('resize', this.onWindowResize.bind(this), false);
-
 	THREE.Scene.call(this);
 };
 FOUR.Scene.prototype = Object.create(THREE.Scene.prototype);
 
 
-FOUR.Scene.prototype.withAddons = function ( components ) {
-
-	// trackball controls
-	this.controls = components.trackballControls?(
-		new THREE.TrackballControls(this.camera)):false;
-
-	// physics with Physijs
-	this.physics = (components.physijs)?{}:undefined;
-
-	return this;
-}
-
-FOUR.Scene.prototype.withShaders = function ( shaders ) {
-
-	for (var s of shaders) {
-		var pass = new FOUR.RenderPass()
-			.resolveScene(this)
-			.addShader(s.resolveScene(this))
-			.addTo(this.renderer);
-	};
-
-	return this;
-}
-
-FOUR.Scene.prototype.init = function () {
-
-	this.renderer.set({
-		'width' : this.width,
-		'height' : this.height,
-		'shadows' : true,
-		'backgroundColor' : this.backgroundColor,
-		'parent' : this.parent
-	})
-
-	this.camera.position.set(0,2,1);
-	this.camera.lookAt(new THREE.Vector3(0,0,0)); 
-	this.initControls();
-	return this;
-}
-
 /**
  *
- * initialize trackball controls
- *
- */
-FOUR.Scene.prototype.initControls = function () {
-	if (this.controls) {
-		this.controls.rotateSpeed = 1.0;
-		this.controls.zoomSpeed = 1.2;
-		this.controls.panSpeed = 0.8;
-		this.controls.noZoom = false;
-		this.controls.noPan = false;
-		this.controls.staticMoving = true;
-		this.controls.dynamicDampingFactor = 0.3;
-		this.controls.keys = [ 65, 83, 68 ];
-		// controls.addEventListener('change', setupBlurredBackground);
-	}
-	return this;
-}
-
-/**
- *
- * render callback
+ * 
  *
  */
 FOUR.Scene.prototype.animate = function () {
-	requestAnimationFrame(this.animate.bind(this));
-	this.controls.update();
-	this.renderer.render(this, this.camera);
-}
+	// requestAnimationFrame(this.animate.bind(this));
+	// this.controls.update();
+	this.renderer.render();
+};
+
 
 /**
  *
- * handle resizing the scene
+ * :param options - array of shaders (inheriting from FOUR.Shader)
  *
  */
-FOUR.Scene.prototype.onWindowResize = function ( event ) {
-	this.camera.aspect = window.innerWidth / window.innerHeight;
-	this.camera.updateProjectionMatrix();
-	this.renderer.setSize(window.innerWidth, window.innerHeight);
-	this.controls.handleResize();
+FOUR.Scene.prototype.withShaders = function ( shaders ) {
+	for (var s of shaders) { this.renderer.addShader(new s(this)); }
+	return this;
+}
+
+
+/**
+ *
+ * :param options - json object with the following options:
+ *
+ * {
+ *		'trackballControls' : boolean,
+ *		'physics' : boolean
+ * }
+ *
+ */
+FOUR.Scene.prototype.withAddons = function ( options ) {
+	return this;
+}
+
+
+/**
+ *
+ * 
+ *
+ */
+FOUR.Scene.prototype.init = function () {
+	this.camera.position.set(0,2,1);
+	this.camera.lookAt(new THREE.Vector3(0,0,0)); 
+	// this.initControls();
 	return this;
 }
